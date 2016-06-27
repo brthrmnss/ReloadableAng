22763141343104bb9fbd1f2d516ed9a73363ec3d
user <morrisonstephen@gmail.com>
2016-06-27 14:05:32 -0400

    dfdk

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -92,6 +92,7 @@ module.exports = function(config) {
       'bower_components/blueimp-gallery/js/blueimp-gallery-youtube.js',
       'bower_components/angular-ui-sortable/sortable.js',
       'bower_components/angular.pubsub/src/angular-pubsub.js',
+      'bower_components/bacon/dist/Bacon.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
